1044c1e174e73d1f3d5549303349323a98b845f8
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2025-08-08 05:39:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export const BOT_TOKEN = ':';
 export const CHANNEL_ID = '@username channel'; // Kosongkan "" jika tidak ingin log ke channel
-export const IMAGE_URL = 'https://www.imgtr.ee/images/2025/06/24/RZ5w.th.jpeg'; // Kosongkan "" jika tidak ingin gambar
-
+export const IMAGE_URL = 'https://imgtree.co/get/8XSZW5.png?v=1842475219'; // Kosongkan "" jika tidak ingin gambar
 export const API_URL = `https://api.telegram.org/bot${BOT_TOKEN}`;
